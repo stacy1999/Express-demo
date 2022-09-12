c08f0c57effd301d361741bdeb72eda29242b05a
stacy1999 <anastasiapolinesi@gmail.com>
2022-09-12 11:07:03 +0100

    Added redis dependency, configured Redis client and added two endpoints, GET and POST to read and write to Redis

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const { createClient } = require('redis')
+
+const client = createClient();
+
+(async () => {
+    await client.connect();
+})();
+
+client.on('connect', () => console.log('::> Redis Client Connected'));
+client.on('error', (err) => console.log('<:: Redis Client Error', err));
+
+
+app.get('/credentials', (req, res) => {
+    const key = req.query.key.toString();
+    client.get(key).then(r => res.send(`The value for the ${key} credential is ${r}`) );
+});
+
+app.post('/credentials/:key', (req, res) => {
+    const key = req.params.key.toString();
+    const value = req.query.value.toString();
+
+    client.set(key, value).then(
+        r => res.send(`Credential successfully added with ${key}: ${value}`))
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`))
